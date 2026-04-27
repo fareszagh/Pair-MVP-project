d04ac70124f216d28c9d2dc9a0497defd39c42f7
fareszagh <fz22211094@gmail.com>
2026-04-27 18:44:10 +0200

    updating Jwt_secret

--- Backend/Middleware/authMiddleware.js
+++ Backend/Middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 const verifyToken = (req , res, next)=>{
     const token = req.headers.authorization.split(" ")[1]
-    const decoded = jwt.verify(token,"secret")
+    const decoded = jwt.verify(token,process.env.JWT_SECRET)
     req.user = decoded
     next()
 }
