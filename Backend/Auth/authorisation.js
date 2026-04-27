9d98e0f4b0895ca652cd536d6d34461cd4b50b59
fareszagh <fz22211094@gmail.com>
2026-04-27 17:53:18 +0200

    auth is done

---
+++ Backend/Auth/authorisation.js
@@ -0,0 +1,76 @@
+import User from "../Models/UserModel.js";
+import bcrypt from "bcrypt";
+import { json } from "express";
+import jwt from "jsonwebtoken";
+
+export const login = async (req , res ) => {
+    try {
+        const {email , password} = req.body;
+
+        //find user
+        const user = await User.findOne({where :{email}})
+        if (!user){
+            return res.status(404).json({message: "User does not exist"})
+        }
+        //check if password match
+        const ismatch = await bcrypt.compare(password,user.password)
+        if (!ismatch) {
+            return res.status(401).json ({message : "password is not correct"})
+        }
+
+        //generate token
+        const token = jwt.sign(
+            {id:user.id,email:user.email},
+            process.env.JWT_SECRET ,
+            {expiresIn: "1d"}
+        )
+
+        res.status(200).json({
+            message:"login successful",
+            token
+        })
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+
+export const register = async (req ,res )=>{
+    try {
+        const {username,email,password}=req.body
+
+        //check if user exist
+        const userexist= await User.findOne({message:{email}})
+        if (userexist){
+            return res.status(400).json({message: "Email exist already"})
+        }
+
+        //hash password
+        const hashedpassword= await bcrypt.hash(password,10)
+
+        //create user
+        const user =await User.create({
+            username,
+            email,
+            password :hashedpassword
+        })
+
+        res.status(201).json({
+            id: user.id,
+            username:user.username,
+            email:user.email
+        })
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+export const getUserById = async (req ,res)=>{
+    try {
+        const getuser= await User.findByPK(req.params.id)
+        res.status(200).json(getuser)
+    } catch (error) {
+        res.status(500)
+        throw new Error(error.message) 
+    }
+}
